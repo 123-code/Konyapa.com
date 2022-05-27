daf5d1a5cb3c3bedea8aaef92a67bc238d17843a
123-code <naranjojose256@gmail.com>
2022-05-27 08:44:39 -0500

    Added Tailwindcss

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = ()=>{
+    const items = ["Registrate","Inicio"];
+    return(
+        <>
+            <h3>Inicio</h3>
+            <h3>Registrate</h3>
+
+    </>
+    )
+}
+export default Navbar;
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from '../Components/Button';
+//src/Components
+
+const Main = ()=>{
+    return(
+        <>
+       <h1> Empieza a vender sin comision </h1>
+
+       <h2> Publica tu anuncio gratis y vende sin comision </h2>
+
+       <h2> Se parte de la comunidad de empresarios mas grande del Ecuador</h2>
+
+       <h2> Preparate para aumentar tus ventas</h2>
+
+         <Button/>  
+
+     
+        
+        </>
+    )
+}
+
+export default Main;
