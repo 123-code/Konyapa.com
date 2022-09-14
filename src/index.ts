f9009d7e261eb9d99d498e05d5003757f0aeda87
123-code <naranjojose256@gmail.com>
2022-09-14 16:15:21 -0500

    switched to ts server

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema, GraphQLObjectType } from "graphql";
+import { Sequelize, Model, DataTypes } from 'sequelize';
+
+const ProductSchema = buildSchema(
+    `type Query{
+        name:String,
+        price:Float,
+        seller:String
+    }`
+);
+
+const RootQuery = new GraphQLObjectType({
+    name:()=>{
+        return 'hello world'    }
+
+}); 
+
+
+ 
+const app = express();
+const port = 5001;
+const sequelize = new Sequelize('sqlite::memory:');
+
+const main = async()=>{
+    app.get("/",(req,res)=>{
+        res.send("Main Route");
+    })
+    
+    
+    app.listen(port,()=>{
+    console.log(`Listening on port ${port}!`);
+    });
+}
+
+main().catch((err)=>{
+    console.error(err);
+})
+
+
+
+ 
+
